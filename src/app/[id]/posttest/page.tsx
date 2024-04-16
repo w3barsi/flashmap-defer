231ac81614e999a76202dc912d8d2d140f1c723d
w3barsi <darcybalaga@gmail.com>
2024-04-16 17:42:03 +0800

    fixed "pre-test" text bug on posttest score page

--- src/app/[id]/posttest/page.tsx
+++ src/app/[id]/posttest/page.tsx
@@ -7,7 +7,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   const [preTest] = await api.quiz.getScore.query({entryId: params.id, testType: "post"})
 
   if(preTest?.score != null){
-    return <div className="flex flex-col h-screen items-center justify-center text-4xl"><h1>Your pre-test score is: {preTest.score}</h1><Link  className="underline hover:underline-offset-4" href="/">Return to home</Link></div>
+    return <div className="flex flex-col h-screen items-center justify-center text-4xl"><h1>Your post-test score is: {preTest.score}</h1><Link  className="underline hover:underline-offset-4" href="/">Return to home</Link></div>
   }
 
 
